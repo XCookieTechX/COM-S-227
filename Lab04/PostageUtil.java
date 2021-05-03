2499cd0affb3149c8bac0bb34b425d83073fc2e9
XCookieTechX <81478514+XCookieTechX@users.noreply.github.com>
2021-05-03 15:32:10 -0500

    Add files via upload

--- Lab04/PostageUtil.java
+++ Lab04/PostageUtil.java
@@ -1,7 +1,7 @@
-package postage3;
+package postage1;
 
 import static java.lang.Math.ceil;
-
+import java.util.Scanner;
 public class PostageUtil
 {
   /**
@@ -13,15 +13,24 @@ public class PostageUtil
    */
   public static double computePostage(double weight)
   {
-    double cost = .47;
+    double cost;
 
-    if (weight > 1){
-      cost = cost + ceil(weight - 1) * .21;
+    if (weight <= 1) {
+      cost = .47;
+    }else{
+      if (weight <= 3.5){
+        cost = .47 + ceil(weight - 1) * .21;
+      }else{
+        cost = .94 + ceil(weight - 1) * .21;
+      }
     }
-    if (weight > 3.5){
-      cost = cost + .47;
-    }
-
     return cost;
+  }
+  public static void main(String[] args)
+  {
+    Scanner scnr = new Scanner(System.in);
+    double weight = scnr.nextDouble();
+
+    System.out.println(computePostage(weight));
   }
 }
