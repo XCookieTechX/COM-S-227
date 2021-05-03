d73327b8e5c4282305b125eb91fe5ef9cb31db7d
XCookieTechX <81478514+XCookieTechX@users.noreply.github.com>
2021-05-03 15:31:30 -0500

    Add files via upload

---
+++ Lab04/PostageUtil.java
@@ -0,0 +1,27 @@
+package postage3;
+
+import static java.lang.Math.ceil;
+
+public class PostageUtil
+{
+  /**
+   * Returns the cost of postage for a letter of the given weight.
+   * @param weight
+   *   given weight in ounces
+   * @return
+   *   cost of postage for the weight
+   */
+  public static double computePostage(double weight)
+  {
+    double cost = .47;
+
+    if (weight > 1){
+      cost = cost + ceil(weight - 1) * .21;
+    }
+    if (weight > 3.5){
+      cost = cost + .47;
+    }
+
+    return cost;
+  }
+}
